e66bba6f974984885642fba139abc9bf51b00f43
Andron1992 <nagola2112@gmail.com>
2024-05-03 19:18:42 +0200

    Домашка 4

--- assistant_bot.py
+++ assistant_bot.py
@@ -1,4 +1,3 @@
-
 def parse_input(user_input):
     cmd, *args = user_input.split()
     cmd = cmd.strip().lower()
@@ -9,7 +8,6 @@ def add_contact(contacts, name, phone_number):
         return "Contact already exists."
     contacts[name] = phone_number
     return "Contact added."
-
 def change_contact(contacts, name, new_phone_number):
     if name not in contacts:
         return "Contact not found."
@@ -32,7 +30,8 @@ def main():
     print("Welcome to the assistant bot!")
     while True:
         user_input = input("Enter a command: ")
-        command, *args = parse_input(user_input)
+        command, args = parse_input(user_input)
+
 
         if command in ["close", "exit"]:
             print("Good bye!")
@@ -45,8 +44,22 @@ def main():
                 continue
             name, phone_number = args
             print(add_contact(contacts, name, phone_number))
+        elif command == "change":
+            if len(args) != 2:
+                print("Invalid number of arguments. Usage: change [ім'я] [новий номер телефону]")
+                continue
+            name, new_phone_number = args
+            print(change_contact(contacts, name, new_phone_number))
+        elif command == "phone":
+            if len(args) != 1:
+                print("Invalid number of arguments. Usage: phone [ім'я]")
+                continue
+            name = args[0]
+            print(show_phone(contacts, name))
+        elif command == "all":
+            print(show_all(contacts))
         else:
             print("Invalid command.")
 
 if __name__ == "__main__":
-    main()
+        main()
